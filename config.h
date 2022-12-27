0366a4e3bc18b5ae706fa6ff9a26337b2c400ef3
Hammas <dev.hammas@gmail.com>
2022-12-27 19:50:11 +0500

    add tag rules

--- config.h
+++ config.h
@@ -27,8 +27,12 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	// { "Gimp",     NULL,       NULL,       0,            1,           -1 },
+	// { "firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Alacritty",NULL,       NULL,       1,            0,           -1 },
+	{ "firefox",  NULL,       NULL,       2,            0,           -1 },
+	{ "Thunar",   NULL,       NULL,       1 << 2,            0,           -1 },
+	{ "vlc",      NULL,       NULL,       1 << 3,            0,           -1 },
 };
 
 /* layout(s) */
